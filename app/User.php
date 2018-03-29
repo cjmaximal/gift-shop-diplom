872040a5837fcc8e724099cb761aa591c0434e64
Igor Ivanov <cjmaximal@gmail.com>
2018-03-29 13:42:50 +0300

    Fix. User model

--- app/User.php
+++ app/User.php
@@ -64,7 +64,7 @@ class User extends Authenticatable
      */
     public function products()
     {
-        return $this->belongsToMany(Product::class);
+        return $this->belongsToMany(Product::class)->withPivot('count');
     }
 
     /**
