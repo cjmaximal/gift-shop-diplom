2b1066b07bbe02c7130582d75ed441467e65b79d
Igor Ivanov <cjmaximal@gmail.com>
2018-04-06 12:26:08 +0300

    Fix. Middleware. CheckOrderHash.

--- app/Http/Middleware/CheckOrderHash.php
+++ app/Http/Middleware/CheckOrderHash.php
@@ -17,15 +17,22 @@ class CheckOrderHash
      */
     public function handle($request, Closure $next)
     {
-        if ($request->filled('hash') == false || $request->filled('email' == false)) {
-            return redirect()->route('home.index');
-        }
+
 
         if (\Auth::check()) {
+            $id = \Route::current()->parameter('id');
+            $order = Order::query()->where('user_id', \Auth::user()->id)->where('id', $id)->first();
+
+            if (!$order) {
+                return redirect()->route('home.index');
+            }
+
             return $next($request);
         }
 
-
+        if ($request->filled('hash') == false || $request->filled('email' == false)) {
+            return redirect()->route('home.index');
+        }
         $order = Order::query()->where('email', $request->input('email'))->first();
         $hash = md5(optional($order)->id . ':' . optional($order)->email);
         if ($hash != $request->input('hash')) {
