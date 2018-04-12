8b7850a817e510a7e2eee904a810096628ef0674
Igor Ivanov <cjmaximal@gmail.com>
2018-04-12 09:47:14 +0300

    Fix. Conditions view.

--- resources/views/conditions.blade.php
+++ resources/views/conditions.blade.php
@@ -2,23 +2,27 @@
 
 @section('title', 'Условия')
 @section('content')
-<div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card card-default">
-                <div class="card-header">Условия</div>
-
+        <div class="col-md-12">
+            @if (session('status'))
+                <div class="alert alert-success">
+                    {{ session('status') }}
+                </div>
+            @endif
+        </div>
+    </div>
+    <div class="row mb-3">
+        <div class="col-md-12">
+            <div class="card border-light" style="box-shadow: rgba(0, 0, 0, .3) 0 0 8px 1px;">
+                <div class="card-header">Условия работы</div>
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    Условия!
+                    <h4 class="card-text">
+                        <span class="oi oi-warning text-warning"></span>
+                        Данный интернет-магазин не осуществляет продажи представленных товаров.
+                    </h4>
+                    <h5 class="card-text">Сайт интернет-магазина разработан в качестве дипломной работы.</h5>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
